9f743772b0bb4c71c34f44066227a9cbc7871daf
Dima <dimitriy648@yandex.ru>
2021-12-22 02:13:11 +0300

    added task23

---
+++ src/ru/mirea/task23/Opt.java
@@ -0,0 +1,20 @@
+package ru.mirea.task23;
+
+public class Opt {
+    public static void main(String[] args) {
+        String labelMenu = "Press s to start";
+        String labelDownMenu1 = "Score: ";
+        String labelDownMenu2 = " Time: ";
+        String labelIcon = "Pacman";
+        String labelIcon2 = "Pacman";
+
+        System.out.println("Хэш-код для Str: " + labelIcon.hashCode());
+        System.out.println("Хэш-код для Str: " + labelMenu.hashCode());
+        System.out.println("Хэш-код для Str: " + labelDownMenu1.hashCode());
+        System.out.println("Хэш-код для Str: " + labelDownMenu2.hashCode());
+
+        System.out.println("Сравнение: " + labelIcon.equals(labelIcon2));
+        System.out.println("Сравнение: " + labelIcon.equals(labelDownMenu2));
+
+    }
+}
